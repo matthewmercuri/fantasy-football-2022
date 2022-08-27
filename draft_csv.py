d5afa8644908aab97f091c286b8d17c71f79fb38
Matthew Mercuri <mercurimatthew@gmail.com>
2022-08-27 13:04:22 -0400

    added VBD to draft csv

--- draft_csv.py
+++ draft_csv.py
@@ -11,6 +11,13 @@ def _apply_fantpt_touch(x, past_players, pp_df):
     return pp_df[pp_df["Player"] == pp_df_player]["FantPt/Touch"].iloc[0]
 
 
+def _apply_vbd(x, past_players, pp_df):
+    current_player = x["Player"]
+    pp_df_player, _ = process.extractOne(current_player, past_players[0])
+
+    return pp_df[pp_df["Player"] == pp_df_player]["Fantasy_VBD"].iloc[0]
+
+
 def assemble_draft_csv() -> pd.DataFrame:
     draft_df = get_fantasy_pros_adp_df()
 
@@ -20,6 +27,8 @@ def assemble_draft_csv() -> pd.DataFrame:
     draft_df["FantPt/Touch"] = draft_df.apply(
         _apply_fantpt_touch, axis=1, args=([past_players], pp_df)
     )
+
+    draft_df["VBD"] = draft_df.apply(_apply_vbd, axis=1, args=([past_players], pp_df))
 
     return draft_df
 
